815965bf47eff88c3788e0ebae8ba22373098da8
Halil Bilgil <hbilgil@gmail.com>
2022-06-28 18:03:11 +0300

    Edit script.js to declare global variables

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//GLOBAL VARIABLE DECLARATIONS
+
+
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+let  circleTurn = false; // Boolean variable & "X" is the game opener mark, so "O" class turn is declared as false
