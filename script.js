329392e3327a390c7176b74743a352fbee113dd8
Halil Bilgil <hbilgil@gmail.com>
2022-06-28 18:07:45 +0300

    Edit script.js to create Modules

--- script.js
+++ script.js
@@ -5,3 +5,24 @@
 const X_CLASS = 'x'
 const O_CLASS = 'o'
 let  circleTurn = false; // Boolean variable & "X" is the game opener mark, so "O" class turn is declared as false
+
+
+//MODULES
+
+
+/*---Display Control Module---*/
+
+
+const displayController = (() => {
+
+
+})();
+
+
+/*---Game Control Module---*/
+
+
+const gameController = (() => {
+
+
+})();
